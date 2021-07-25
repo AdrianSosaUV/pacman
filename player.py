f4703c86cbf8f4e7e7a996bcb650c17719c1d19b
Adrian Sosa <adrsosa@uv.mx>
2021-07-24 21:19:25 -0500

    Add Entity super class

--- player.py
+++ player.py
@@ -1,61 +1,48 @@
-from settings import *
+from entity import *
 
 
-class Player:
+class Player(Entity):
 
     def __init__(self, app, pos):
-        self.app = app
-        self.grid_pos = pos
-        self.pix_pos = self.get_pix_pos()
-        self.direction = vec(1, 0)
-        self.stored_direction = None
+        super().__init__(app, pos, PLAYER_COLOUR)
         self.able_to_move = True
         self.current_score = 0
+        self.speed = 2
+        self.lives = 3
 
     def update(self):
         if self.able_to_move:
-            self.pix_pos += self.direction
+            self.pix_pos += self.direction * self.speed
         if self.verify_move():
             if self.stored_direction != None:
                 self.direction = self.stored_direction
             self.able_to_move = self.can_move()
-
-        # setting grid position in reference to pix pos
-        self.grid_pos[0] = (self.pix_pos[0]-TOP_BOTTOM_BUFFER +
-                            self.app.cell_width//2)//self.app.cell_width+1
-        self.grid_pos[1] = (self.pix_pos[1]-TOP_BOTTOM_BUFFER +
-                            self.app.cell_height//2)//self.app.cell_height+1
+        self.set_grid_pos()
         if self.on_coin():
             self.eat_coin()
 
     def draw(self):
-        pygame.draw.circle(self.app.screen, PLAYER_COLOUR, (int(
-            self.pix_pos.x), int(self.pix_pos.y)), self.app.cell_width//2-2)
+        self.draw_character()
+        self.draw_lifes()
 
     def on_coin(self):
         if self.grid_pos in self.app.coins:
-            return self.verify_move()
+            if int(self.pix_pos.x+TOP_BOTTOM_BUFFER//2) % self.app.cell_width == 0:
+                if self.direction == vec(1, 0) or self.direction == vec(-1, 0):
+                    return True
+            if int(self.pix_pos.y+TOP_BOTTOM_BUFFER//2) % self.app.cell_height == 0:
+                if self.direction == vec(0, 1) or self.direction == vec(0, -1):
+                    return True
         return False
 
     def eat_coin(self):
         self.app.coins.remove(self.grid_pos)
         self.current_score += 100
 
-    def move(self, direction):
-        self.stored_direction = direction
-
-    def get_pix_pos(self):
-        return vec((self.grid_pos.x*self.app.cell_width)+TOP_BOTTOM_BUFFER//2+self.app.cell_width//2,
-                   (self.grid_pos.y*self.app.cell_height)+TOP_BOTTOM_BUFFER//2+self.app.cell_height//2)
-
-    def verify_move(self):
-        if int(self.pix_pos.x + TOP_BOTTOM_BUFFER//2) % self.app.cell_width == 0:
-            if self.direction == vec(1, 0) or self.direction == vec(-1, 0):
-                return True
-
-        if int(self.pix_pos.y + TOP_BOTTOM_BUFFER//2) % self.app.cell_height == 0:
-            if self.direction == vec(0, 1) or self.direction == vec(0, -1):
-                return True
+    def draw_lifes(self):
+        for x in range(self.lives):
+            pygame.draw.circle(self.app.screen, CHERRY,
+                               (30 + 20 * x, HEIGHT - 15), self.radius)
 
     def can_move(self):
         for wall in self.app.walls:
